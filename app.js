b5f5a62305b921ba1d4ec0a3c90bf0bb8884edf6
TalhaShaikh150 <tssmart233@gmail.com>
2025-05-17 05:00:29 +0500

    Local Storage With Delete Functionality

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
 let NotesList = JSON.parse(localStorage.getItem("NotesList")) || [];
 
-/Show Modal/;
-renderNote();
+loadPages();
 
-const buttonElement = document.querySelector(".js-add-note");
-const modalElement = document.querySelector(".modal");
-const overLay = document.querySelector(".overlay");
-displayModal();
-function displayModal() {
-  buttonElement.addEventListener("click", () => {
-    modalElement.classList.add("display");
-    overLay.classList.add("display");
-  });
+function loadPages() {
+  const modalElement = document.querySelector(".modal");
+  const overLay = document.querySelector(".overlay");
+  const saveButton = document.querySelector(".js-save");
 
-  closeModal(modalElement, overLay);
-}
-
-/Close Modal/;
-function closeModal(modalElement, overLay) {
-  const closeButton = document.querySelector(".fa-xmark");
-  closeButton.addEventListener("click", () => {
+  saveButton.addEventListener("click", () => {
+    addNote();
     modalElement.classList.remove("display");
     overLay.classList.remove("display");
   });
+
+  renderNote();
+  displayModal(modalElement, overLay);
+  closeModal(modalElement, overLay);
 }
 
-/Add Note/;
-const saveButton = document.querySelector(".js-save");
+/* This Function Is To Display Modal */
+function displayModal(modalElement, overLay) {
+  const buttonElement = document.querySelector(".js-add-note");
+  const wholeBody = document.body;
+
+  buttonElement.addEventListener("click", () => {
+    modalElement.classList.add("display");
+    overLay.classList.add("display");
+    wholeBody.addEventListener("keydown", (event) => {
+      function hide() {
+        modalElement.classList.remove("display");
+        overLay.classList.remove("display");
+      }
+      if (event.key === "Escape") {
+        hide();
+      }
+    });
+  });
+}
+
+/* This Function Is To Close Modal */
+function closeModal(modalElement, overLay) {
+  const closeButton = document.querySelector(".fa-xmark");
+
+  closeButton.addEventListener("click", hide);
+  function hide() {
+    modalElement.classList.remove("display");
+    overLay.classList.remove("display");
+  }
+}
+
 function addNote() {
   const titleInput = document.querySelector(".js-title");
   const categoryInput = document.querySelector(".js-category");
@@ -36,38 +58,56 @@ function addNote() {
   const category = categoryInput.value;
   const content = contentInput.value;
 
-  NotesList.push({ title, category, content });
+  NotesList.unshift({ title, category, content });
+
   renderNote();
-  modalElement.classList.remove("display");
-  overLay.classList.remove("display");
+
   titleInput.value = "";
   categoryInput.value = "";
   contentInput.value = "";
+
   saveToStorage();
 }
-saveButton.addEventListener("click", addNote);
 
 function renderNote() {
   const noteContainer = document.querySelector(".notes-container");
   let html = "";
+
   for (let i = 0; i < NotesList.length; i++) {
     let noteObject = NotesList[i];
     const { title, category, content } = noteObject;
-    html += `<div class="note">
-          <div class="note-head">
-     <h3>${title}</h3>
-            <i class="fa-solid fa-trash"></i>
-          </div>
-          <div class="flex-col">
-            <p class="content">${content}</p>
-          </div>
-          <div class="flex">
-            <button class="category">${category}</button>
-            <p>Date</p>
-          </div>
-        </div>`;
+
+    html += `  
+            <div class="note">
+                <div class="note-head">
+                    <h3>${title}</h3>
+                    <i class="fa-solid fa-trash" data-index=${i}></i>
+                </div>
+                <p class="content">${content}</p>
+
+                <div class="note-head">
+                    <button class="category-btn">${category}</button>
+                    <p>Date</p>
+                </div>
+            </div>
+        `;
   }
+
   noteContainer.innerHTML = html;
+  deleteNote();
+}
+
+function deleteNote() {
+  let deleteIcon = document.querySelectorAll(".fa-trash");
+
+  deleteIcon.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      let indexnum = icon.dataset.index;
+      NotesList.splice(indexnum, 1);
+      saveToStorage();
+      renderNote();
+    });
+  });
 }
 
 function saveToStorage() {
